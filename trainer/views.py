76baf92a0309da88bae88f4def460aa76c1f8de4
Viktoria Moskalenko <vicky1234555617@gmail.com>
2025-01-04 11:57:48 +0000

    Lesson 16

--- trainer/views.py
+++ trainer/views.py
@@ -1,3 +1,39 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+import trainer
+
+
 # Create your views here.
+def category_page(request):
+    return HttpResponse("<h1>Welcome to my category page</h1>")
+
+def trainer_page(request, trainer_id):
+    if request.method == 'GET':
+        service_categories = trainer.models.Category.objects.all()
+        my_services = trainer.models.Service.objects.filter(trainer = request.user).all()
+        return render(request,"trainer.html", {"categories": service_categories, "services": my_services}) #form with adding service
+
+    return HttpResponse("<h1>Welcome to my trainer page</h1>")
+
+def trainer_service_page(request, user_id, service_id):
+    return HttpResponse("<h1>Welcome to my trainer service page</h1>")
+
+def service_page(request):
+    if request.method == 'POST':
+        form_data = request.POST
+        service_cat = trainer.models.Category.objects.get(pk=form_data['category'])
+
+        service = trainer.models.Service(
+            level=form_data['level'],
+            duration =form_data['duration'],
+            price=form_data['price'],
+            category=service_cat,
+            trainer=request.user,
+        )
+        service.save()
+    return HttpResponse("<h1>Welcome to my service page</h1>")
+
+def booking_for_user(request):
+    return HttpResponse("<h1>Welcome to my booking page</h1>")
+
